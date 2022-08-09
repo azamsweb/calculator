25094866a3259cbba16b8e0980e3a8b8c06f7e2a
Mohammed Azam Ahmed <mdazam1611@gmail.com>
2022-08-09 20:21:11 +0530

    first commit

---
+++ calC.js
@@ -0,0 +1,15 @@
+function display(value){
+document.getElementById("result").value += value;
+}
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+function calculate() {
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
+}
+function back() {
+  var value = document.getElementById("result").value;
+  document.getElementById("result").value = value.substr(0, value.length - 1);
+}
